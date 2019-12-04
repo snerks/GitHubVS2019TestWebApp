47ca4ab817332c30d6a1da003265694dce21005d
snerks <martin.github@outlook.com>
2019-12-04 20:21:36 +0000

    Dummy change

--- GitHubTestWebApp/Controllers/ValuesController.cs
+++ GitHubTestWebApp/Controllers/ValuesController.cs
@@ -10,6 +10,7 @@ namespace GitHubTestWebApp.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
